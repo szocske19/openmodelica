c3a5ba0367682f470bea7f8f6b222903a69292ce
Krisztián <szocske19@gmail.com>
2017-12-12 09:38:41 +0100

    tree traversal once at the end

--- org.modelica.mdt.ui/src/onlab/openmodelica/own/implementation/of/mdt/LevelNode.java
+++ org.modelica.mdt.ui/src/onlab/openmodelica/own/implementation/of/mdt/LevelNode.java
@@ -2,6 +2,8 @@
 package onlab.openmodelica.own.implementation.of.mdt;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import openmodelica.MoClass;
 
@@ -11,14 +13,16 @@ public class LevelNode
 	private ArrayList<LevelNode> children;
 	private int level;
 	private MoClass cp;
+	private boolean alreadyCalled;
 
-	public LevelNode(MoClass cp, LevelNode parent, int level){
-		this.level = level;
+	public LevelNode(MoClass cp, LevelNode parent, boolean isFunction){
+		this.level = -1;
 		this.cp = cp;
 		this.parents = new ArrayList<LevelNode>();		
 		this.children = new ArrayList<LevelNode>();
+		alreadyCalled = false;
 		
-		if(parent != null){
+		if(parent != null && !isFunction){
 			this.parents.add(parent);
 			parent.children.add(this);
 		}
@@ -31,26 +35,62 @@ public class LevelNode
 
 	public int getLevel()
 	{
+		if(level != -1){
+			return level;
+		}
+		if(!alreadyCalled){
+			setLevel();
+		}
 		return level;
 	}
 	
-	public void raiseLevelAndChangeParent(LevelNode newParent, int newLevel){
-		if(newParent != null){
-			parents.add(newParent);
-			newParent.children.add(this);
-		}
-		raiseLevel(newLevel);
-	}	
-
-	private void raiseLevel(int newLevel){
-		System.out.println("cp: " + cp.getName() + ", old level: " + level + ", new level: " + newLevel );
-		if(newLevel > level){
-			level = newLevel;
-			for (LevelNode child : children)
-			{
-				child.raiseLevel(newLevel+1);			
+	public void setLevel(){
+		alreadyCalled = true;
+		List<Integer> parentsLevel = new ArrayList<Integer>();
+		for (LevelNode parent : parents){
+			if(parent != null){
+				int x = parent.getLevel();
+				if(!children.contains(parent)){
+					x += 1;
+				}
+				parentsLevel.add(x);
 			}
 		}
+		
+		if(parentsLevel.size() == 0){
+			level = 0;
+		} else {
+			Collections.sort(parentsLevel);
+			level = parentsLevel.get(parentsLevel.size()-1);
+		}				
 	}
+	
+	public void addParent(LevelNode newParent){
+		if(newParent != null){
+			if(!newParent.parents.contains(this)){
+				parents.add(newParent);
+				newParent.children.add(this);
+			} else {
+				throw new IllegalArgumentException("Circular reference: Child = " + cp.getName() + " newParent = " + newParent.cp.getName());
+			}
+		}
+//		if(newLevel > level){
+//			System.out.println("raiseLevel:");
+//			raiseLevel(newLevel);
+//		}
+	}
+	
+
+//	private void raiseLevel(int newLevel){
+//		System.out.println("cp: " + cp.getName() + ", old level: " + level + ", new level: " + newLevel );
+//		if(newLevel > level){
+//			level = newLevel;
+//			for (LevelNode child : children)
+//			{
+//				System.out.println("Child of " + cp.getName());
+//				child.raiseLevel(newLevel+1);			
+//			}
+//		}
+//	}
 
 }
